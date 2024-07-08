756f4272fffb34e69d4e26e729dd5656d6a67310
Yash <kumaryashcse@gmail.com>
2024-07-08 20:20:22 +0530

    script template

---
+++ script/token-drop/benchmark-claim/claimLegacy.s.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script, console} from "forge-std/Script.sol";
+import {LibClone} from "lib/solady/src/utils/LibClone.sol";
+
+contract BenchmarkLegacyTokenDrop_Claim is Script {
+    function setUp() public {}
+
+    function run() public {
+        vm.startBroadcast();
+
+        vm.stopBroadcast();
+    }
+}
